c1fdc371e2c57b97dfe69807f3847b0efdaaf987
retro98boy <retro98boy@qq.com>
2025-11-02 22:44:58 +0800

    custom-build update

--- custom-build/docker-run.mk
+++ custom-build/docker-run.mk
@@ -7,7 +7,7 @@ DOCKER_IMAGE_TAG := latest
 
 define DOCKER_RUN
 	$(DOCKER) run -it --init --rm \
-	-v $(ROOT_DIR):/work \
+	-v $(ROOT_DIR):/workspace \
 	-u 1000:1000 \
 	$(DOCKER_IMAGE):$(DOCKER_IMAGE_TAG)
 endef
