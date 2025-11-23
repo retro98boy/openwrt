42fe3149befd7a02403b908c723c3d2e7a453b02
retro98boy <retro98boy@qq.com>
2025-11-23 20:13:28 +0800

    airoha: an7581: Enable USB by default

--- package/kernel/linux/modules/usb.mk
+++ package/kernel/linux/modules/usb.mk
@@ -1864,6 +1864,7 @@ define KernelPackage/usb3
 	+TARGET_bcm53xx:kmod-phy-bcm-ns-usb3 \
 	+TARGET_ramips_mt7621:kmod-usb-xhci-mtk \
 	+TARGET_econet_en751221:kmod-usb-xhci-mtk \
+	+TARGET_airoha_an7581:kmod-usb-xhci-mtk \
 	+TARGET_mediatek:kmod-usb-xhci-mtk
   KCONFIG:= \
 	CONFIG_USB_PCI=y \
